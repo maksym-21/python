832881d72d70992382536d1930d391b124451fea
maksym-21 <mikalandzelo2@gmail.com>
2021-12-06 22:35:27 +0100

    task8_6 changes

--- zestaw8/task8_6.py
+++ zestaw8/task8_6.py
@@ -1,19 +1,33 @@
-def P(i:int, j:int) :
-    matrix = [[0 for x in range(10)] for x in range(5)]
+matrix = { (0, 0) : 0.5 }
 
-    if i == j == 0 : 
-        matrix[i][j] = 0.5
-        return matrix[i][j]
-    elif i > 0 and j == 0 : 
-        matrix[i][j] = 0.0
-        return matrix[i][j]
-    elif j > 0 and i == 0 : 
-        matrix[i][j] = 1.0
-        return matrix[i][j]
-    elif i > 0 and j > 0 : 
-        matrix[i][j] = 0.5 * (P(i-1, j) + P(i, j-1))
-        return matrix[i][j]
-    else : raise ValueError('Something went wrong:(')
+def P(i, j) :
+    if i < 0 or j < 0 : raise Exception("Arguments must be greater or equal to zero")
+    if (i, j) in matrix.keys() : return matrix.get((i, j))
+
+    if i > 0 and j == 0 : return 0.0
+    elif i == 0 and j > 0 : return 1.0
+    elif i > 0 and j > 0 :
+        matrix[(i, j)] = 0.5 * (P(i - 1, j) + P(i, j - 1))
+
+        return matrix.get((i, j))
+
+#
+#def P(i:int, j:int) :
+#    matrix = [[0 for x in range(10)] for x in range(5)]
+#
+#    if i == j == 0 : 
+#        matrix[i][j] = 0.5
+#        return matrix[i][j]
+#    elif i > 0 and j == 0 : 
+#        matrix[i][j] = 0.0
+#        return matrix[i][j]
+#    elif j > 0 and i == 0 : 
+#        matrix[i][j] = 1.0
+#        return matrix[i][j]
+#    elif i > 0 and j > 0 : 
+#        matrix[i][j] = 0.5 * (P(i-1, j) + P(i, j-1))
+#        return matrix[i][j]
+#    else : raise ValueError('Something went wrong:(')
 
 
 print(P(0,0)) # 0.5
