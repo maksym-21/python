0eb9d67f94ceb4e8160485bd7aa90a0ec12f6776
maksym-21 <mikalandzelo2@gmail.com>
2021-12-12 22:44:03 +0100

    task 9_6 changes

--- zestaw9/task9_6.py
+++ zestaw9/task9_6.py
@@ -4,25 +4,20 @@ class Node:
         self.left = None
         self.right = None
 
-class BinaryTree:
+def count_leafs(top) : 
+    if top is None : return 0
+    elif top.left is None and top.right is None : return 1
+    else : return count_leafs(top.left) + count_leafs(top.right)
 
-    def __init__(self,other) :
-        self.parent = other
+def count_total(top) :
+    if top is None : return 0
+    elif top.right is None and top.left is None : return 1
+    else : return 1 + count_total(top.right) + count_total(top.left)
 
-    def count_leafs(self,top) : 
-        if top is None : return 0
-        elif top.left is None and top.right is None : return 1
-        else : return self.count_leafs(top.left) + self.count_leafs(top.right)
-
-    def count_total(self,top) :
-        if top is None : return 0
-        elif top.right is None and top.left is None : return 1
-        else : return 1 + self.count_total(top.right) + self.count_total(top.left)
-
-    def calc_total(self,top) -> int :
-        if top is None : return 0
-        elif top.right is None and top.left is None : return top.data
-        else : return top.data + self.calc_total(top.left) + self.calc_total(top.right)
+def calc_total(top) -> int :
+    if top is None : return 0
+    elif top.right is None and top.left is None : return top.data
+    else : return top.data + calc_total(top.left) + calc_total(top.right)
 
 if __name__ == '__main__' :
     main = Node(4)
@@ -35,11 +30,8 @@ if __name__ == '__main__' :
     main.right.right = Node(1)
     main.right.left = Node(1)
 
-
-    binary = BinaryTree(main)
-
-    print(binary.calc_total(main))
-    print(binary.count_total(main))
-    print(binary.count_leafs(main))
+    print(calc_total(main))
+    print(count_total(main))
+    print(count_leafs(main))
 
 
